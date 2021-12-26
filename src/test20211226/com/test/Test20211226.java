846fbcbf5a127e488ce9ce682a5b2448ac8a75af
JaeUk Lee <87742894+wook2002@users.noreply.github.com>
2021-12-26 15:13:31 +0900

    test3:13

--- src/test20211226/com/test/Test20211226.java
+++ src/test20211226/com/test/Test20211226.java
@@ -3,7 +3,7 @@ package test20211226.com.test;
 public class Test20211226 {
 
 	public static void main(String[] args) {
-		System.out.println("test2:59");
+		System.out.println("test3:12");
 	}
 
 }
