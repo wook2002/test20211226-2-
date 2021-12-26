d0c48050d65b699dd056b815c3c10a43d506c442
JaeUk Lee <87742894+wook2002@users.noreply.github.com>
2021-12-26 16:49:20 +0900

    eeejjj

--- src/test20211226/com/test/Test20211226.java
+++ src/test20211226/com/test/Test20211226.java
@@ -5,6 +5,7 @@ public class Test20211226 {
 	public static void main(String[] args) {
 		System.out.println("test3:12");
 		System.out.println("test3:12");
+		System.out.println("test4:49");
 	}
 
 }
